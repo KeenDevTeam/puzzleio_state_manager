12ad4560b96115baef735889ad553530e0dc74c6
Danial Manavi <dmanavi@live.com>
2020-10-08 12:55:21 +0330

    InMemory storage driver.

---
+++ src/storage/memory.ts
@@ -0,0 +1,162 @@
+/**
+ * Memory (In-Memory) storage
+ */
+
+import { ApplicationError, } from '@speedup/error';
+import { generate as generateId } from 'shortid';
+
+import { StorageAdapter } from '../type/storage-adapter';
+
+const errors = {
+
+    noFlowId: {
+        code: 'E_NO_FLOW_ID',
+        message: 'flowId must be a non-empty string.',
+    }
+};
+
+export type MemoryStateModel = {
+
+    stack: Array<any>,
+
+    createdAt: Date,
+    updatedAt: Date,
+}
+
+export class MemoryStorage implements StorageAdapter {
+
+    /**
+     * Application state
+     */
+    private readonly state: { [key: string]: MemoryStateModel } = {};
+
+    /**
+     * Initialize the storage engine
+     */
+    async initialize(): Promise<void> { }
+
+    /**
+     * Add state for a new flow (initial state)
+     * @param state Initial state of the flow
+     */
+    async save<T>(state: T): Promise<string | void>;
+
+    /**
+     * Set the current state of a flow
+     * @param state Current state
+     * @param flowId Flow ID
+     */
+    async save<T>(state: T, flowId?: string): Promise<string | void> {
+
+        let isNew = false;
+
+        if (!flowId) {
+            flowId = await this.generateFlowId();
+            isNew = true;
+        }
+
+        const currentDate = new Date();
+
+        if (isNew) {
+
+            this.state[flowId] = {
+
+                stack: [],
+
+                createdAt: currentDate,
+                updatedAt: currentDate,
+            };
+        }
+
+        this.state[flowId].stack.push(state);
+        this.state[flowId].updatedAt = currentDate;
+
+        // return newly created flow's ID
+        if (isNew) { return flowId; }
+    }
+
+    /**
+     * Load a specific flow state
+     * @param flowId Target flow ID
+     */
+    async load<T>(flowId: string): Promise<T | null>;
+    /**
+     * Get a single item from the stack of a specific flow
+     * @param flowId Target flow ID
+     * @param index Target item index
+     */
+    async load<T>(flowId: string, index?: number): Promise<T | null> {
+
+        if (!flowId) {
+            throw new ApplicationError({
+                ...errors.noFlowId,
+            });
+        }
+
+        // make sure that the index is neither null or undefined nor 0
+        if (!index) { index = 0; }
+
+        const state = this.state[flowId];
+        if (state === undefined) { return null; }
+
+        return state.stack.reverse()[index];
+    }
+
+    /**
+     * Get total items in the stack for a specific flow
+     * @param flowId Target flow ID
+     */
+    async getStackSize(flowId: string): Promise<number> {
+
+        if (!flowId) {
+            throw new ApplicationError({
+                ...errors.noFlowId,
+            });
+        }
+
+        const state = this.state[flowId];
+        if (state === undefined) { return 0; }
+
+        return state.stack.length;
+    }
+
+    /**
+     * Get a flow history
+     * @param flowId Target flow ID
+     */
+    async history(flowId: string): Promise<Array<any>> {  // eslint-disable-line @typescript-eslint/no-explicit-any
+
+        if (!flowId) {
+            throw new ApplicationError({
+                ...errors.noFlowId,
+            });
+        }
+
+        const state = this.state[flowId];
+        if (state === undefined) { return []; }
+
+        return [...state.stack.reverse()];
+    }
+
+    /* Private methods */
+
+    /**
+     * Generate a new unique flow ID
+     */
+    private async generateFlowId(): Promise<string> {
+
+        let flowId = '';
+        let isUnique = false;
+
+        while (!isUnique) {
+
+            // generate a new random ID
+            flowId = generateId();
+
+            // check ID uniqueness
+            isUnique = this.state[flowId] === undefined;
+        }
+
+        return flowId;
+    }
+}
---
+++ test/memory.spec.ts
@@ -0,0 +1,143 @@
+/**
+ * Memory storage tests
+ */
+
+import 'mocha';
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+import { MemoryStorage } from '../src/storage/memory';
+import { StorageAdapter } from '../src/type/storage-adapter';
+
+chai.use(chaiAsPromised);
+
+describe('[PuzzleIO][StateManager][Storage][Memory]', () => {
+
+	describe('initialize', () => {
+
+		it('should successfully called', async () => {
+
+			const instance = new MemoryStorage();
+
+			expect(instance).to.be.instanceOf(MemoryStorage);
+			expect(instance.initialize()).to.be.eventually.eq(undefined);
+		});
+
+	});
+
+	describe('save', () => {
+
+		const instance: StorageAdapter = new MemoryStorage();
+		let testFlowId: string | void = '';
+
+		describe('success', () => {
+
+			it('should save a new item when flowId is missing', async () => {
+
+				expect(instance).to.be.instanceOf(MemoryStorage);
+
+				testFlowId = await instance.save<number>(0);
+				expect(testFlowId).to.be.a('string');
+
+				const stackSize = await instance.getStackSize(testFlowId + '');
+				expect(stackSize).to.be.a('number').that.is.eq(1);
+			});
+
+			it('should update current state using the previously added flowId', async () => {
+
+				expect(instance).to.be.instanceOf(MemoryStorage);
+
+				await instance.save<number>(1, testFlowId + '');
+				expect(testFlowId).to.be.a('string');
+
+				const stackSize = await instance.getStackSize(testFlowId + '');
+				expect(stackSize).to.be.a('number').that.is.eq(2);
+			});
+
+		});
+	});
+
+	describe('load', () => {
+
+		it('should return null when the key does not exist', async () => {
+
+			const instance: StorageAdapter = new MemoryStorage();
+			expect(instance).to.be.instanceOf(MemoryStorage);
+
+			const state = await instance.load<number>('invalid-id');
+			expect(state).to.be.null;
+		});
+
+		it('should return current value when the key exists', async () => {
+
+			const instance: StorageAdapter = new MemoryStorage();
+			expect(instance).to.be.instanceOf(MemoryStorage);
+
+			const flowId = await instance.save<number>(0) as string;
+
+			const currentValue = await instance.load<number>(flowId);
+			expect(currentValue).to.be.a('number').that.is.eq(0);
+
+			await instance.save<number>(currentValue! + 1, flowId); // eslint-disable-line @typescript-eslint/no-non-null-assertion
+			const updatedValue = await instance.load<number>(flowId);
+			expect(updatedValue).to.be.a('number').that.is.eq(1);
+		});
+	});
+
+	describe('getStackSize', () => {
+
+		it('should return 0 when the key does not exist', async () => {
+
+			const instance: StorageAdapter = new MemoryStorage();
+			expect(instance).to.be.instanceOf(MemoryStorage);
+
+			const state = await instance.getStackSize('invalid-id');
+			expect(state).to.be.a('number').that.is.eq(0);
+		});
+
+		it('should return current stack size when the key exists', async () => {
+
+			const instance: StorageAdapter = new MemoryStorage();
+			expect(instance).to.be.instanceOf(MemoryStorage);
+
+			const flowId = await instance.save<number>(0) as string;
+			const currentStackSize = await instance.getStackSize(flowId);
+			expect(currentStackSize).to.be.a('number').that.is.eq(1);
+
+			await instance.save<number>(1, flowId);
+			const updatedStackSize = await instance.getStackSize(flowId);
+			expect(updatedStackSize).to.be.a('number').that.is.eq(2);
+		});
+
+	});
+
+	describe('history', () => {
+
+		it('should return empty array when the key does not exist', async () => {
+
+			const instance: StorageAdapter = new MemoryStorage();
+			expect(instance).to.be.instanceOf(MemoryStorage);
+
+			const history = await instance.history('invalid-id');
+			expect(history).to.be.an('array').that.has.lengthOf(0);
+		});
+
+		it('should return current stacked items when the key exists', async () => {
+
+			const instance: StorageAdapter = new MemoryStorage();
+			expect(instance).to.be.instanceOf(MemoryStorage);
+
+			const flowId = await instance.save<number>(0) as string;
+			const currentHistory = await instance.history(flowId);
+			expect(currentHistory).to.be.an('array').that.has.lengthOf(1);
+			expect(currentHistory[0]).to.be.a('number').that.is.eq(0);
+
+			await instance.save<number>(1, flowId);
+			const updatedHistory = await instance.history(flowId);
+			expect(updatedHistory).to.be.an('array').that.has.lengthOf(2);
+			expect(updatedHistory[0]).to.be.a('number').that.is.eq(1);
+			expect(updatedHistory[1]).to.be.a('number').that.is.eq(0);
+		});
+
+	});
+});
